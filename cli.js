62f36459cd9297cb30ef9bfb91b0aa3fedd2fd38
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-07-02 16:47:54 -0300

    criando linha de comendo com md-links

--- cli.js
+++ cli.js
@@ -1,17 +1,9 @@
 #!/usr/bin/env node
 
-const mdLinks = require('commander');
-const package = require('./package.json');
+const mdLinks = require('./index.js');
+const [, , ...args] = process.argv;
 
-mdLinks.version(package.version);
+mdLinks(args)
 
-//add comando na CLI
-mdLinks
-  .command('add <check>')
-  .description('Checando links do Markdown')
-  .action((check) => {
-    console.log(check);
-  });
 
-//A process.argv retorna uma matriz que contém os argumentos da linha de comando transmitidos quando o processo Node.js. for iniciado.
-mdLinks.parse(process.argv);
+//https://x-team.com/blog/a-guide-to-creating-a-nodejs-command/
