aeab4693f02f8f923891bb9447b90028665d9041
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-07-01 22:22:53 -0300

    que patifaria, estressada

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-module.exports = () => {
-  // ...
-};
+const mdLinks = require('express');
+
+
+const app = mdLinks();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3001);
+
+// module.exports = () => {
+//   // ...
+// };
