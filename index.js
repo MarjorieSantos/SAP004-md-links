048804fe1f33b47754f7b923ebb0d43b161efa3a
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-07-03 14:03:12 -0300

    add promiesse

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // #!/usr/bin/env node
 
 const fs = require('fs');
+const { resolve } = require('path');
+const { rejects } = require('assert');
 const arr = [];
 
 //err - retorno de algum erro;
@@ -10,21 +12,23 @@ const arr = [];
 //gm =>  g => acha todas as correspondências em vez de parar após achar a primeira m => operar sobre múltiplas linhas, e não apenas o começo ou fim de toda a string de entrada
 
 const mdLinks = (path) => {
-  fs.readFile(path[0], (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      const mdString = data.toString();
-      const regex = /[^!]\[(.[^\]]*)\]\(([^]\S+)\)/gm;
-      const checkLink = mdString.match(regex);
-      checkLink.forEach((links) => {
-        const href = links.match(/\(([^]\S+)\)/)[1];
-        const text = links.match(/(.[^\]]*)/)[1].replace('[', '');
-        const file = path[0].replace('[]', '')
-        arr.push({ href, text, file });
-      })
-      console.log(arr);
-    }
+  return new Promise((resolve, reject) => {
+    fs.readFile(path[0], (err, data) => {
+      if (err) {
+        reject(console.log(err))
+      } else {
+        const mdString = data.toString();
+        const regex = /[^!]\[(.[^\]]*)\]\(([^]\S+)\)/gm;
+        const checkLink = mdString.match(regex);
+        checkLink.forEach((links) => {
+          const href = links.match(/\(([^]\S+)\)/)[1];
+          const text = links.match(/(.[^\]]*)/)[1].replace('[', '');
+          const file = path[0].replace('[]', '')
+          arr.push({ href, text, file });
+        })
+        resolve(console.log(arr));
+      };
+    });
   });
 };
 
