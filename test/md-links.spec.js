0c9b801e02ac7afa7eebfe71c64f8d4b3d6a1df7
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-07-03 13:50:58 -0300

    testando se é uma função

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,7 +1,8 @@
-// const mdLinks = require('../');
+const mdLinks = require('../index.js');
+// const { parseExpectedArgs } = require('commander');
 
-// describe('mdLinks', () => {
-//   it('should...', () => {
-//     console.log('FIX ME!');
-//   });
-// });
+describe('mdLinks', () => {
+  it('should be a function', () => {
+    expect(typeof mdLinks).toBe('function');
+  });
+});
