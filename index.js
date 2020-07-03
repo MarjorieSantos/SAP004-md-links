bcc4c97e0da33eaab300679889bd5a26fb402800
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-07-03 18:10:16 -0300

    resolvendo testes async

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const fs = require('fs');
 const arr = [];
 
-//err - retorno de algum erro;
-//data - conteúdo do arquivo;
-//path => arquivo de extenção .md
-
-//gm =>  g => acha todas as correspondências em vez de parar após achar a primeira m => operar sobre múltiplas linhas, e não apenas o começo ou fim de toda a string de entrada
-
 const mdLinks = (path) => {
   return new Promise((resolve, reject) => {
     fs.readFile(path[0], 'utf8', (err, data) => {
       if (err) {
         reject(err);
       } else {
+        console.log(path);
         const mdString = data.toString();
         const regex = /\[(.[^\]]*)\]\((http.*)\)/gm;
         const checkLink = mdString.match(regex);
@@ -28,6 +23,6 @@ const mdLinks = (path) => {
   });
 };
 
-//=> match  : [1], para retornar a segunda(indice2) do array que estava sendo retornado, com index, input, groups, etc. pegando assim apenas o link certinho
-
 module.exports = mdLinks;
+
+//=> match  : [1], para retornar a segunda(indice2) do array que estava sendo retornado, com index, input, groups, etc. pegando assim apenas o link certinho
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,8 +1,27 @@
 const mdLinks = require('../index.js');
-// const { parseExpectedArgs } = require('commander');
 
 describe('mdLinks', () => {
   it('should be a function', () => {
     expect(typeof mdLinks).toBe('function');
   });
 });
+
+describe('the links are okay', () => {
+  it('should be return a link, the title of the link and the file', () => {
+    expect(mdLinks('./mock.md')).resolves.toEqual([
+      { href: 'https://thewalkingdead.com.br/', text: 'The Walking Dead' },
+      { href: 'http://www.adorocinema.com/series/serie-7330/temporada-16736/', text: '1ª temporada' },
+      { href: 'http://www.adorocinema.com/series/serie-7330/temporada-18736/', text: '2ª temporada' },
+      { href: 'http://www.adorocinema.com/series/serie-7330/temporada-20603/', text: '3ª temporada' },
+      { href: 'http://www.adorocinema.com/series/serie-7330/temporada-22184/', text: '4ª temporada' },
+      { href: 'http://www.adorocinema.com/series/serie-7330/temporada-23750/', text: '5ª temporada' }
+    ]);
+  })
+});
+
+// describe('incorrect link', () => {
+//   it('should return an error if the link is incorrect', () => {
+//     expect(mdLinks('./test/mock.md')).rejects.toEqual('Link não encontrado')
+//   });
+// });
+
