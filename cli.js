fb85ff8bf202480da2ba6b144cb611cbcd041f34
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-07-06 17:33:51 -0300

    arrumando seta para links

--- cli.js
+++ cli.js
@@ -6,13 +6,12 @@ const [, , ...args] = process.argv;
 mdLinks(args)
   .then((result) => {
     result.forEach((archive) => {
-      const link = `${archive.href} ${archive.stats ? '  ➨   ' + archive.stats : ''} ➨  ${archive.text} ➨  ${archive.file} \n`
+      const link = `${archive.href} ${archive.stats ? ' ➨   ' + archive.stats : ''} ➨  ${archive.text} ➨  ${archive.file} \n`
       console.log(link);
     })
   })
-  .catch((err) => {
-    console.log(err)
-    console.log('não existem links nesse arquivo!');
+  .catch((erro) => {
+    throw new Error(`${erro}  'não existem links nesse arquivo!'`)
   });
 
 //https://x-team.com/blog/a-guide-to-creating-a-nodejs-command/
